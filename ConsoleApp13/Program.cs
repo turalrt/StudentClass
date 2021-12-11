abd2ec2256bdf93925f564fd1edf2f8f57bb366a
Tural Taghiyev <turalrt@code.edu.az>
2021-12-11 21:24:49 +0400

    first commit

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student("Tural Taghiyev","\nEngineering","\nGeological Engineering","\nMETU","\nturaltagiyev@yahoo.com","\n+994554990808");
+            stu.Info();
+        }
+    }
+}
---
+++ ConsoleApp13/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Student
+    {
+        public string FullName;
+        public string Course;
+        public string Subject;
+        public string University;
+        public string Email;
+        public string PhoneNumber;
+
+        public Student(string fullname, string course, string subject, string university, string email, string phonenumber)
+        {
+            FullName = fullname;
+            Course = course;
+            Subject = subject;
+            University = university;
+            Email = email;
+            PhoneNumber = phonenumber;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"{FullName} {Course} {Subject} {University} {Email} {PhoneNumber}");
+        }
+
+    }
+}
